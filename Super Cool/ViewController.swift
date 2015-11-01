01c5bacfce2226c600f9579b102860c21386673e
Scott Schneier <schneier@optonline.net>
2015-11-01 17:44:34 -0500

    Initial Commit

---
+++ Super Cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Cool
+//
+//  Created by Scott Schneier on 11/1/15.
+//  Copyright © 2015 Scott Schneier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
